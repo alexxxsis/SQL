952b17b28d390000f88a3b3de773622d87fe26b1
alexxxsis <144637900+alexxxsis@users.noreply.github.com>
2024-01-15 01:07:38 +0800

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -8,12 +8,12 @@ INSERT INTO Employees(Name,Gender) VALUES
 ('Сандан', 'М'),
 ('Золто', 'М'),
 ('Золто', 'М');
-CREATE TABLE Employees2(
+CREATE TABLE Employees_2(
 ID INT PRIMARY KEY,
 Name CHAR(255),
 Gender CHAR(255)
 );
-INSERT INTO Employees(ID,Name,Gender) VALUES
+INSERT INTO Employees_2(ID,Name,Gender) VALUES
 (1,'Алексей', 'М'),
 (2,'Сандан', 'М'),
 (3,'Золто', 'М'),
