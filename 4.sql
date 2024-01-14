30258ca5fd9c5aa76ef4b17b67b8d50445c35c6a
alexxxsis <144637900+alexxxsis@users.noreply.github.com>
2024-01-15 00:51:33 +0800

    Create 4.sql

---
+++ 4.sql
@@ -0,0 +1,8 @@
+//4 задание
+UPDATE Posts SET Caption = 'Новый кибертрак' WHERE ID = 1;
+UPDATE Posts SET Caption = 'Новый год' WHERE ID = 3;
+UPDATE Projects SET Description = 'Ура прорыв' WHERE ID = 5;
+UPDATE Projects SET Description = 'Нашли решение' WHERE ID = 7;
+UPDATE Testimonials SET Text = 'Круто сделали' WHERE ID = 4;
+UPDATE Testimonials SET Text = '10/10' WHERE ID = 8;
+
